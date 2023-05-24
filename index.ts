ad3273bf7f9a17ba33c78f24ce3b63ab2a9ab846
Evan Kim <evan977512@gmail.com>
2023-05-23 21:39:51 -0600

    First time wity TypeScript

---
+++ index.ts
@@ -0,0 +1,8 @@
+// number, string, boolean, null,  undefined, any, void, never, object, array, tuple, enum, alias
+
+let a: number = 3;
+a = 4;
+let b: string = "hello";
+
+let c: any = 4;
+c = "hello"; // no error...?
