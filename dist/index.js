d2aa0b479683874acdbba004cd6488b69deed942
Evan Kim <evan977512@gmail.com>
2023-05-23 22:30:34 -0600

    tsconfig.json file created

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,19 @@
 // number, string, boolean, null, undefined, any, void, never, object, array, tuple, enum, alias
-
-let a: number = 3;
+let a = 3;
 a = 4;
-let b: string = "hello";
-
-let c: any = 4;
+let b = "hello";
+let c = 4;
 c = "hello"; // no error...?
 c = true; // no error...?
-
 // string or number?
-let d: number | string = "eiifofof";
-
-let e: string[] = ["apple", "banana", "orange"];
+let d = "eiifofof";
+let e = ["apple", "banana", "orange"];
 e.push("grape");
 // e.push(4); // error
-
 // functions
-function addNumber(a: number, b: number): number {
-  return a + b;
+function addNumber(a, b) {
+    return a + b;
 }
 addNumber(11, 42);
-
 // control + shift + b to build
+//# sourceMappingURL=index.js.map
